301b14f85002e1499552ac8f3309326db66b1bdd
Alexey <77731459+goooodnes@users.noreply.github.com>
2022-05-13 22:20:01 +0300

    Update coho.js

--- coho.js
+++ coho.js
@@ -45,9 +45,6 @@ window.onload = async () => {
         low: 0.03,
         average: 0.03,
         high: 0.03,
-    },
-    features: [
-        "cosmwasm", "ibc-transfer", "ibc-go", "wasmd_0.24+"
-    ],    
+    },   
 });
 }
